bfc6f1f8c7186446d1ffa8bc0c32d0a855ae7db5
GAURAV GOSWAMI <gouravgoswami48@gmail.com>
2020-05-23 19:12:49 +0530

    eventID model addition

--- models/event.js
+++ models/event.js
@@ -29,6 +29,13 @@ const eventSchema = new mongoose.Schema({
     totalTickets: {
         type: Number,
         required: true
+    },
+    eventId: {
+        type: String,
+        trim: true,
+        required: true,
+        index: true,
+        unique: true
     }
 }, {
     collection: 'event'
